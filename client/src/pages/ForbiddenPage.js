1ca1e37c9ad6c54365fa619e2073fef5e0ab8a47
poom2548 <poompookpoo2909@gmail.com>
2026-09-13 21:13:19 +0700

    chore: remove compiled js files

--- client/src/pages/ForbiddenPage.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-export function ForbiddenPage() {
-    return (_jsxs("div", { className: "forbidden-container", children: [_jsx("h1", { children: "Access Denied" }), _jsx("p", { children: "You do not have permission to view this page." })] }));
-}
